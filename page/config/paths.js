62f5048076669c2b7620542d245a9e4a8a9d7e6e
FedeG <federicogonzalez761@gmail.com>
2018-01-25 02:39:16 -0300

    Add page for show all calcos

--- page/config/paths.js
+++ page/config/paths.js
@@ -9,7 +9,7 @@ const url = require('url');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
-const envPublicUrl = process.env.PUBLIC_URL;
+const envPublicUrl = 'https://flisol-argentina.github.io/calcos/';
 
 function ensureSlash(path, needsSlash) {
   const hasSlash = path.endsWith('/');
